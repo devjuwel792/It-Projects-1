ed8d942a1744ad43bc5b6b0b30e7adfd4907f9b5
Juwel <developerjuwel792@gmail.com>
2023-09-20 01:01:55 +0600

    home page done

---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+    /* Testimonial Slider Start */
+    $('.responsive').slick({
+        dots: true,
+        infinite: true,
+        speed: 1500,
+        
+        autoplay: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+});
